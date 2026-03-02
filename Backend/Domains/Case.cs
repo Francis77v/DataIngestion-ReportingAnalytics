a7a2036b00437e3c8cfde0264f3ff76966d5eb72
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-03-02 21:32:48 +0800

    add annotation on model fields

--- Backend/Domains/Case.cs
+++ Backend/Domains/Case.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.Domains;
 
 public class Case
 {
    public int Id { get; set; }
+   [MaxLength(100)]
    public required string RegistryNumber { get; set; }
+   [MaxLength(100)]
    public required string CaseFileNumber { get; set; }
+   [MaxLength(100)]
    public required string CaseCategory { get; set; }
    public Client? ClientNavigation { get; set; }
 }
--- Backend/Domains/Client.cs
+++ Backend/Domains/Client.cs
@@ -1,25 +1,39 @@
-using Backend.Infrastructure.enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Backend.Domains;
 
 public class Client
 {
     public int Id { get; set; }
+    [MaxLength(100)]
     public required string Sex { get; set; }
+    [MaxLength(100)]
     public required string Address { get; set; }
+    [MaxLength(100)]
     public required string MaritalStatus { get; set; }
+    [MaxLength(100)]
     public required string Occupation { get; set; }
+    [MaxLength(100)]
     public required string AgeBracket { get; set; }
+    [MaxLength(100)]
     public required string EducationalAttainment { get; set; }
+    [MaxLength(100)]
     public required string Religion { get; set; }
+    [MaxLength(100)]
     public string? IfAfterCare { get; set; } = null!;
-    public required string IfDdeEnum { get; set; } 
+    [MaxLength(100)]
+    public required string IfDde { get; set; } 
+    [MaxLength(100)]
     public required string Nationality { get; set; }
+    [MaxLength(100)]
     public required string Ethnicity { get; set; }
+    [MaxLength(100)]
     public required string LivingArrangement { get; set; }
+    [MaxLength(100)]
     public required string SubstanceUse { get; set; }
+    [MaxLength(100)]
     public required string Program { get; set; }
     
-
+    public int CaseId { get; set; }
     public Case? CaseNavigation { get; set; } = null!;
 }
