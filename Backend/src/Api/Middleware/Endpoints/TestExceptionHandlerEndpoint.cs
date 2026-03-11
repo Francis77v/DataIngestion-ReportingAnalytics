190cae21a5319612a4cc700182f61fad6083a241
Francis Joseph Flores <floresfrancisjoseph28@gmail.com>
2026-03-11 20:57:08 +0800

    create endpoint for testing exception handler errors

---
+++ Backend/src/Api/Middleware/Endpoints/TestExceptionHandlerEndpoint.cs
@@ -0,0 +1,29 @@
+using Backend.Domain.Exceptions;
+
+namespace Backend.Api.Middleware.Endpoints;
+
+public static class TestExceptionHandlerEndpoint
+{
+    public static void ExceptionHandlerEndpoint(this WebApplication app)
+    {
+        app.MapGet("/500", async () =>
+        {
+            throw new Exception("Something went wrong on the server.");
+        });
+        
+        app.MapGet("/404", async () =>
+        {
+            throw new NotFoundException("not found bro");
+        });
+        
+        app.MapGet("/400", async () =>
+        {
+            throw new BadRequestException("bad request bro");
+        });
+        
+        app.MapGet("/401", async () =>
+        {
+            throw new UnauthorizedException("unauthorized bro");
+        });
+    }
+}
